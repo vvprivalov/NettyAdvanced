d2293ec0917e68f7833e63fd087393e73b1b8081
vvprivalov <vv.privalov@yandex.ru>
2022-04-13 15:11:40 +0300

    Добавил класс AuthMessage.

---
+++ src/main/java/common/message/AuthMessage.java
@@ -0,0 +1,19 @@
+package common.message;
+
+public class AuthMessage extends Message{
+    private String login;
+    private String password;
+
+    public AuthMessage(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
